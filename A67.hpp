413312dded243d8639247b0da67cbd96444bcd46
UNIDRA <57227914+UNIDRA@users.noreply.github.com>
2022-10-11 19:53:47 -0700

    A67
    The program uses 3 swap functions that it overloads. The first swap function uses a temporary variable in order to switch two values that it has been provided with. When swap has 3 inputs, it checks the maximum, minimum, and middle value using couple if statements that compares if the number is larger, smaller, or in the middle of the previous number. When the swap function has 4 inputs, the functions swaps the values using the 2 input swap function 3 times. It does this in a specific way that moves all the variables left by a step and moves the first variable to the last spot.

--- A67.hpp
+++ A67.hpp
@@ -7,3 +7,48 @@ using namespace std;
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void swap(int &a, int &b, int &c) {
+    int max = 0;
+    int min = 100;
+    int mid;
+    if (max < a) {
+        max = a;
+    }
+    if (max < b) {
+        max = b;
+    }
+    if (max < c) {
+        max = c;
+    }
+    if (min > a) {
+        min = a;
+    }
+    if (min > b) {
+        min = b;
+    }
+    if (min > c) {
+        min = c;
+    }
+    if (a != min || a != max) {
+        a = mid;
+    }
+    if (b != min || b != max) {
+        b = mid;
+    }
+    if (c != min || c != max) {
+        c = mid;
+    }
+    a = min;
+    b = mid;
+    c = max;
+}
+void swap(int &a, int &b, int &c, int &d) {
+    swap(a, d);
+    swap(a, c);
+    swap(a, b);
+}
